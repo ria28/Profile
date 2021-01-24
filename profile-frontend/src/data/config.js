73a549a4d29d5bf29a6bda3a5f921e9ef7e9c356
RIA <riajaiswal28@gmail.com>
2021-01-24 12:07:12 +0000

    add all

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Ria Jaiswal",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Ria Jaiswal !",
+  githubUrl: "https://github.com/ria28",
+  linkedinUrl: "https://www.linkedin.com/in/ria-jaiswal-a57466195/",
+  cvLink: "https://drive.google.com/file/d/1-smDajJ64o8TTPsylBHiTy0rHdr3kBnQ/view?usp=sharing",
+};
